ba55f863f9a281700be13b970b0b0e2f8e22d5de
RahulSharma-1799 <2020ucp1799@mnit.ac.in>
2023-07-29 16:38:51 +0530

    deploy

--- client/src/components/Todos.jsx
+++ client/src/components/Todos.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { deleteTodo, getAllTodos } from "../redux/actions/index.js";
 import { useDispatch, useSelector } from "react-redux";
-import { ALL_TODOS,ACTIVE_TODOS,DONE_TODOS } from "../redux/actions/type.js";
+import { ALL_TODOS, ACTIVE_TODOS, DONE_TODOS } from "../redux/actions/type.js";
 //component
 import Todo from "./Todo.jsx";
 import Tabs from "./Tabs.jsx";
@@ -16,16 +16,14 @@ const Todos = () => {
   const getTodos = () => {
     if (currentTab === ALL_TODOS) {
       return todos;
+    } else if (currentTab === ACTIVE_TODOS) {
+      return todos.filter((todo) => !todo.done);
+    } else if (currentTab === DONE_TODOS) {
+      return todos.filter((todo) => todo.done);
     }
-    else if (currentTab === ACTIVE_TODOS) {
-      return todos.filter(todo=>!todo.done)
-    }
-    else if (currentTab === DONE_TODOS) {
-      return todos.filter(todo=>todo.done)
-    }
-  }
+  };
   const removeDoneTodos = () => {
-    todos.forEach(({done, _id}) => {
+    todos.forEach(({ done, _id }) => {
       if (done) {
         dispatch(deleteTodo(_id));
       }
@@ -36,15 +34,19 @@ const Todos = () => {
       <article>
         <div>
           <Tabs currentTab={currentTab} />
-          { 
-            todos.some(todo => todo.done) ? (
-              <button className="btn clear" onClick={removeDoneTodos}>Remove Done Todos</button>
-            ):null
-          }
+          {{
+            if(todos) {
+              todos.some((todo) => todo.done) ? (
+                <button className="btn clear" onClick={removeDoneTodos}>
+                  Remove Done Todos
+                </button>
+              ) : null;
+            },
+          }}
         </div>
         <ul>
           {getTodos().map((todo) => (
-              <Todo key={todo._id} todo={todo} />
+            <Todo key={todo._id} todo={todo} />
           ))}
         </ul>
       </article>
